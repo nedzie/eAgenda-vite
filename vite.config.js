ca80a6628c6d2ad7b7afb84030b38efe684bc88b
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-08-19 14:04:59 -0300

    perai

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         index: resolve(root, "index.html"),
         tarefaList: resolve(root, "tarefas/tarefa.list.html"),
         tarefaCreate: resolve(root, "tarefas/tarefa.create.html"),
-        contatoList: resolve(root, "contatos/contato.create.html"),
+        contatoList: resolve(root, "contatos/contato.list.html"),
         contatoCreate: resolve(root, "contatos/contato.create.html"),
         itemList: resolve(root, "tarefas/item.list.html"),
         itemCreate: resolve(root, "tarefas/item.create.html")
       }
     }
   },
-  publicDir: "../public/"
+  publicDir: "../public"
 });
